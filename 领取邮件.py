a7e83f4c33e10d14badd59212b6f74fb183869c1
小渔 <864529498@qq.com>
2026-05-27 19:20:12 +0800

    1

--- 领取邮件.py
+++ 领取邮件.py
@@ -73,7 +73,7 @@ def window_screenshot(hwnd):
 
 def main():
     # 1. 初始化窗口句柄 (保持你的三层逻辑)
-    hwnd_outer = win32gui.FindWindow("LDPlayerMainFrame", "墨族")
+    hwnd_outer = win32gui.FindWindow("LDPlayerMainFrame", "国家攻城1队")
     if not hwnd_outer:
         print("错误: 找不到模拟器主窗口")
         return
